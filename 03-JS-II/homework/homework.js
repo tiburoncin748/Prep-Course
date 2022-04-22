c659414f4e6a79cc59cb215a3d05bfc6469455eb
tiburoncin748 <lauty.pombo@gmail.com>
2022-04-22 00:43:34 -0300

    probando JS2 2 ejer

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -18,6 +18,11 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad>=18){
+    return "Allowed";
+  }else{
+    return "Not allowed";
+  }
 }
   
 function conection(status) {
