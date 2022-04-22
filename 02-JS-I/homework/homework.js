6ae419a51285cd635afe8af61fe8686c863a3825
tiburoncin748 <lauty.pombo@gmail.com>
2022-04-21 21:41:28 -0300

    jdjsd

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -163,7 +163,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  return(Math.random);
+  return Math.random();
 }
 
 function esPositivo(numero) {
