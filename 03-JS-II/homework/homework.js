dda358a99705ee8e53f86582df246baada03db8b
tiburoncin748 <lauty.pombo@gmail.com>
2022-04-22 01:17:18 -0300

    probando JS2 6 ejer

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -85,7 +85,13 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  return (numero === 10 || numero === 5);
+  if(numero === 10){
+    return true;
+  }else if(numero === 5){
+    return true;
+  } else {
+    return false;
+  }
   
 }
 
