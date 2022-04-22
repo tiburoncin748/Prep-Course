13655a3e04a8dbc5c6864642e1bfc963b30d6297
tiburoncin748 <lauty.pombo@gmail.com>
2022-04-22 01:37:20 -0300

    probando JS2 12 ejer

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -112,14 +112,10 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  switch (numero){
-  case (numero % 3 === 0):
-    return "fizz";
-  case (numero % 5 === 0):
-    return "buzz";
-  case (numero % 3 === 0 && numero % 5 === 0):
-    return "fizzbuzz";
-    }
+  if(numero % 3 === 0) return "fizz";
+  if(numero % 5 === 0) return "buzz";
+  if(numero % 15 === 0) return "fizzbuzz";
+  return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
