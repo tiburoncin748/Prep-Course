5d8984a5437e20904a74b97d4cb74e5d50aea5dd
tiburoncin748 <lauty.pombo@gmail.com>
2022-04-22 17:52:37 -0300

    Tarea III 2do ejer

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -10,6 +10,7 @@ function devolverPrimerElemento(array) {
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return (array[array.lenght-1]);
 }
 
 
