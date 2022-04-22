046677f1d0f79be387c07573c24690e64cce6cc7
tiburoncin748 <lauty.pombo@gmail.com>
2022-04-22 00:59:38 -0300

    probando JS2 4 ejer

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -66,6 +66,19 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+
+  switch (color){
+    case 'blue':
+      return("This is blue");
+    case 'red':
+      return("This is red");
+    case 'green':
+      return("This is green");
+    case 'orange':
+      return("This is orange");
+    default:
+      return("Color not found");
+  }
 }
 
 function esDiezOCinco(numero) {
