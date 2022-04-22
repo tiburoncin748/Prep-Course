890359cb0a21dc264da1f837391e89c85edde8b7
tiburoncin748 <lauty.pombo@gmail.com>
2022-04-22 18:09:29 -0300

    Tarea III 5to ejer

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -39,6 +39,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 
