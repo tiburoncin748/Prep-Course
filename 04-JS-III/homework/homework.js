e07db26893a552573b22be7da9f0a2408ea8c4fd
tiburoncin748 <lauty.pombo@gmail.com>
2022-04-25 19:44:09 -0300

    JS III ejer. 10

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -94,14 +94,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  let promedio = 0;
-  let suma = 0;
-  for (var i = 0; i < resultadosTest.length; i++){
-    suma = suma + resultadosTest[i];
-  }
-  promedio = suma / (resultadosTest.length - 1);
-
-  return promedio;
+  return agregarNumeros(resultadosTest) / resultadosTest.length;
 }
 
 
@@ -109,6 +102,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var mayor = numeros [0];
+  for (let i=1; i< numeros.length; i++){
+    if (numeros [i] > mayor){
+      mayor = numeros [i];
+    }
+  }
+  return mayor;
 }
 
 
@@ -116,6 +116,11 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length < 1) return 0;
+  let total = 1;
+  for (var i = 0; i < numeros.length; i++){
+    total = total * numeros[i];}
+    return total;
 }
 
 
